419e4cd5089e10a88bafa4d9c9a5755c768de8a0
John Freeman <john@t3kniq.com>
2026-03-17 22:24:51 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve 'li

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useEffect } from 'react';
-import Link from 'link';
+import Link from 'next/link';
 import Image from 'next/image';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
